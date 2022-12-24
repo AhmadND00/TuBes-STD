25a0d527897f8a1d2e9c62b6c068bde89161543c
Ahmad ND <AhmadND00@protonmail.com>
2022-12-24 05:25:34 -0800

    Updated mll.cpp

--- mll.cpp
+++ mll.cpp
@@ -20,7 +20,12 @@ adrNegara newNegara(infotypeNegara X){
 
 //
 void insertFirstNegara(listNegara &LN, adrNegara P){
-
+    if (first(LN) == NULL){
+        first(LN) = P;
+    } else {
+        next(P) = first(LN);
+        first(LN) = P;
+    }
 
 }
 
@@ -31,26 +36,73 @@ void insertLastNegara(listNegara &LN, adrNegara P){
 }
 
 //
-void insertAfterNegara(listNegara &LN, adrNegara P){
+void insertAfterNegara(listNegara &LN, adrNegara Prec, adrNegara P){
 
 
 }
 
 
-// Input negara pada list negara sesusai dengan urutan mereka di piala dunia
+// FIXME Input negara pada list negara sesusai dengan urutan mereka di piala dunia
 void insertAscNegara(listNegara &LN, adrNegara P){
+    adrNegara Q = first(LN);
+    if (first(LN) == NULL){
+        first(LN) = P;
+        next(P) = NULL;
+    } else if (info(P).finish < info(Q).finish) {
+        insertFirstNegara(LN, P);
+    } else {
+        while (info(P).finish > info(Q).finish){
+            Q = next(Q);
+        }
+        insertAfterNegara(LN, Q, P);
+
+    }
+
+}
+
+adrNegara findNegara(listNegara LN, string namaNegara){
+    adrNegara P = first(LN);
+    adrNegara found = NULL;
+    while (P != NULL && found == NULL){
+        if (info(P).namaNegara == namaNegara){
+            found = P;
+        }
+        P = next(P);
+    }
+    return found;
 
 
 }
 
+//
+float avrAge(listNegara LN, string namaNegara){
+    int totAge = 0;
+    int N;
+
+    adrNegara P = findNegara(LN, namaNegara);
+    if (P == NULL){
+        cout << "Negara yang dicari tidak ada dalam list";
+    } else {
+        if (nextChild(P) != NULL){
+            adrPemain Q = nextChild(P);
+            while (Q != NULL){
+                totAge = totAge + info(Q).umur;
+                N++;
+                Q = next(Q);
+            }
+        }
+    
+    }
+
+    return (float(totAge) / float(N));
+}
+
+// 
 void printNegara(listNegara LN){
-    cout<<"List Negara"<<endl;
+    cout<<"========================== List Negara ==========================\nPosisi \t Negara \t Konfiderasi \t Total Menang Piala Dunia"<<endl;
     adrNegara P = first(LN);
     while (P != NULL){
-        cout<<"Negara: "<<info(P).namaNegara<<endl;
-        cout<<"Konfiderasi: "<<info(P).konfiderasi<<endl;
-        cout<<"Posisi: "<<info(P).finish<<endl;
-        cout<<"Total Menang Piala Dunia: "<<info(P).totJuara<<endl;
+        cout<<info(P).finish<<"\t "<<info(P).namaNegara<<"\t "<<info(P).konfiderasi<<"\t "<<info(P).totJuara<<endl;
         P = next(P);
     }
     
