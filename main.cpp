4255dfab1824604c80c662b479815b8f86550c48
Ahmad ND <AhmadND00@protonmail.com>
2022-12-25 19:17:48 -0800

    created manual input for negara (temporary) to check if the list is working or not

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 int main(){
     //Deklarasi Variabel
     listNegara LN;
-    infotypeNegara X;
+    infotypeNegara xN;
+    infotypePemain xP;
     adrNegara pNegara;
+    adrPemain pPemain;
     int N;
 
     //Create Empty List Negara
@@ -26,34 +28,45 @@ int main(){
     }
     */
     // Manual Input
-    X.namaNegara = "Argentina";
-    X.konfiderasi = "CONMEBOL";
-    X.finish = 1;
-    X.totJuara = 3;
-    pNegara = newNegara(X);
+    xN.namaNegara = "Argentina";
+    xN.konfiderasi = "CONMEBOL";
+    xN.finish = 1;
+    xN.totJuara = 3;
+    pNegara = newNegara(xN);
     insertFirstNegara(LN, pNegara);
-    X.namaNegara = "Perancis";
-    X.konfiderasi = "UEFA   ";
-    X.finish = 2;
-    X.totJuara = 2;
-    pNegara = newNegara(X);
+    xN.namaNegara = "Perancis";
+    xN.konfiderasi = "UEFA";
+    xN.finish = 2;
+    xN.totJuara = 2;
+    pNegara = newNegara(xN);
     insertFirstNegara(LN, pNegara);
-    X.namaNegara = "Kroasia";
-    X.konfiderasi = "UEFA   ";
-    X.finish = 3;
-    X.totJuara = 0;
-    pNegara = newNegara(X);
+    xN.namaNegara = "Kroasia";
+    xN.konfiderasi = "UEFA";
+    xN.finish = 3;
+    xN.totJuara = 0;
+    pNegara = newNegara(xN);
     insertFirstNegara(LN, pNegara);
-    X.namaNegara = "Maroco ";
-    X.konfiderasi = "CONCACAF";
-    X.finish = 4;
-    X.totJuara = 0;
-    pNegara = newNegara(X);
+    xN.namaNegara = "Maroco";
+    xN.konfiderasi = "CONCACAF";
+    xN.finish = 4;
+    xN.totJuara = 0;
+    pNegara = newNegara(xN);
     insertFirstNegara(LN, pNegara);
-
     
     printNegara(LN);
 
+    // Manual Input Pemain
+    xP.no = 10;
+    xP.posisi = "FWD";
+    xP.namaPemain = "Lionel Messi";
+    xP.umur = 35;
+    xP.klub = "PSG";
+    xP.totGoal = 7;
+    pPemain = newPemain(xP);
+    insertPemain(LN, pPemain, "Argentina");
+
+    
+    printListPemain(LN);
 
     return 0;
 }
