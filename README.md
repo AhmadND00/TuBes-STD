8b26fc4e58d70d3df72d0e10c1da49326bc592c3
Ahmad ND <AhmadND00@protonmail.com>
2022-12-27 00:39:02 -0800

    Updated README and Main Menu

--- README.md
+++ README.md
@@ -4,7 +4,19 @@ Project ini dilaksanakan untuk memenuhi pengerjaan tugas besar mata kuliah struk
 ## Tentang Project
 Kami akan menginplementasi Multi Linked List 1 to N untuk statistik data pemain dan negara pada Piala Dunia
 
-![](https://i.imgur.com/wofSSHa.png)<br>(Visualisasi Multi Linked List)
+![](https://i.imgur.com/wofSSHa.png)
+<p align="center">Visualisasi Multi Linked List</p>
+## Guide on How to Use
+
+- You'll be greeted with this menu when you first run the program
+  - ![](https://i.imgur.com/zp5aqgI.png)
+  - Type `1` into the terminal to input the data first (or else, the list is still NULL)
+- Upon typing `1`, you'll then greeted by this screen 
+  - ![](https://i.imgur.com/4m2z8Fy.png)
+  - Type `1` first, then type `2` to input the data into the list
+  - **Note**: The input of Insert Negara is already coded into the main files, meanwhile the input for Input Pemain needed to be manually added by the user (Although, as of now it's the same as Insert Negara)
+- Upon returning to the main menu, you now can chose the other option on the menu to show how the other function works
+
 
 ## To-Do
 - [X] Create List Negara
@@ -18,11 +30,11 @@ Kami akan menginplementasi Multi Linked List 1 to N untuk statistik data pemain 
   - [ ] Delete First (Syafa) // Delete seperti di single linked list biasa
   - [ ] Delete Last (Syafa) // Delete seperti di single linked list biasa
   - [ ] Delete After (Syafa) // Delete seperti di single linked list biasa
-- [ ] Insert Pemain
-  - [ ] Insert First
-  - [ ] Insert Last
-  - [ ] Insert After
+- [ ] Insert Pemain (Needed to be recheked, the procedure shows some weird output)
 - [ ] Delete Pemain 
+- [ ] Function: Find the average players age of each nation
+- [ ] Function: Which country amongs the top 4 scores the most goals in the world cup?
+- [ ] Create a list that shows the top goal scorer in world cup with descending sort
 - [X] Print Negara
 - [X] Print Pemain
 
--- main.cpp
+++ main.cpp
@@ -22,61 +22,67 @@ int main(){
             cout<<"Pilih no: ";
             cin>>choice;
             cout<<"\033[2J\033[1;1H";
-            if (choice == 1){
-                xN.namaNegara = "Argentina";
-                xN.konfiderasi = "CONMEBOL";
-                xN.finish = 1;
-                xN.totJuara = 3;
-                pNegara = newNegara(xN);
-                insertFirstNegara(LN, pNegara);
-                xN.namaNegara = "Perancis";
-                xN.konfiderasi = "UEFA";
-                xN.finish = 2;
-                xN.totJuara = 2;
-                pNegara = newNegara(xN);
-                insertFirstNegara(LN, pNegara);
-                xN.namaNegara = "Kroasia";
-                xN.konfiderasi = "UEFA";
-                xN.finish = 3;
-                xN.totJuara = 0;
-                pNegara = newNegara(xN);
-                insertFirstNegara(LN, pNegara);
-                xN.namaNegara = "Maroco";
-                xN.konfiderasi = "CONCACAF";
-                xN.finish = 4;
-                xN.totJuara = 0;
-                pNegara = newNegara(xN);
-                insertFirstNegara(LN, pNegara);
-                cout<<"Negara berhasil diinputkan ke dalam list!"<<endl<<endl;
-                break;
-            } else if (choice == 2){
-                xP.no = 10;
-                xP.posisi = "FWD";
-                xP.namaPemain = "Lionel Messi";
-                xP.klub = "PSG";
-                xP.umur = 35;
-                xP.totGoal = 7;
-                pPemain = newPemain(xP);
-                insertPemain(LN, pPemain, "Argentina");
-                cout<<"Pemain berhasil diinputkan ke dalam list negara!"<<endl<<endl;
-                break;
-            } else {
-                break;
+            while (choice != 0){
+                if (choice == 1){
+                    xN.namaNegara = "Argentina";
+                    xN.konfiderasi = "CONMEBOL";
+                    xN.finish = 1;
+                    xN.totJuara = 3;
+                    pNegara = newNegara(xN);
+                    insertFirstNegara(LN, pNegara);
+                    xN.namaNegara = "Perancis";
+                    xN.konfiderasi = "UEFA";
+                    xN.finish = 2;
+                    xN.totJuara = 2;
+                    pNegara = newNegara(xN);
+                    insertFirstNegara(LN, pNegara);
+                    xN.namaNegara = "Kroasia";
+                    xN.konfiderasi = "UEFA";
+                    xN.finish = 3;
+                    xN.totJuara = 0;
+                    pNegara = newNegara(xN);
+                    insertFirstNegara(LN, pNegara);
+                    xN.namaNegara = "Maroco";
+                    xN.konfiderasi = "CONCACAF";
+                    xN.finish = 4;
+                    xN.totJuara = 0;
+                    pNegara = newNegara(xN);
+                    insertFirstNegara(LN, pNegara);
+                    cout<<"Negara berhasil diinputkan ke dalam list!"<<endl<<endl;
+                } else if (choice == 2){
+                    xP.no = 10;
+                    xP.posisi = "FWD";
+                    xP.namaPemain = "Lionel Messi";
+                    xP.klub = "PSG";
+                    xP.umur = 35;
+                    xP.totGoal = 7;
+                    pPemain = newPemain(xP);
+                    insertPemain(LN, pPemain, "Argentina");
+                    cout<<"Pemain berhasil diinputkan ke dalam list negara!"<<endl<<endl;
+                }
+                menuInsert();
+                cout<<"Pilih no: ";
+                cin>>choice;
+                cout<<"\033[2J\033[1;1H";
             }
+            break;
         case 2:
             menuShow(); 
             cout<<"Pilih no: ";
             cin>>choice;
             cout<<"\033[2J\033[1;1H";
-            if (choice == 1){
-                printNegara(LN);
-                break;
-            } else if (choice == 2){
-                printListPemain(LN);
-                break;
-            } else {
-                break;
+            while (choice != 0){
+                if (choice == 1){
+                    printNegara(LN);
+                } else if (choice == 2){
+                    printListPemain(LN);
+                } 
+                menuShow();
+                cout<<"Pilih no: ";
+                cin>>choice;
+                cout<<"\033[2J\033[1;1H";
             }
+            break;
         case 3:
             cout<<"Fungsi belum dibuat";
             break;
